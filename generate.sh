f8531dd1631ac5a5fa0ccd8f91a6b02121dafedd
Mattia Oldani <mattiaoldani6@gmail.com>
2023-07-23 18:11:06 +0200

    Aggiunta creazione del file .dat

--- generate.sh
+++ generate.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Generazione file data.dat
+python form.py
+
+# Generazione file turni.pdf
 cp template.md copy.md
 
 python main.py
 pandoc -o turni.pdf template.md --pdf-engine=weasyprint
 
+# Pulizia finale
 cp copy.md template.md
 rm copy.md
 
