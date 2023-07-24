c6ffe3f91bca34a0a0243572a7ff4a1b374d75ef
Mattia Oldani <mattiaoldani6@gmail.com>
2023-07-24 11:42:56 +0200

    Modificato script di generazione dei turni

--- generate.sh
+++ generate.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
 # Generazione file data.dat
+cd script
+mv *.py form
+cd form
+
 python form.py
 
 # Generazione file turni.pdf
+cd ..
+mv form/days.py form/turns.py form/data.dat template
+cd template
 cp template.typ copy.typ
 
-python main.py
+python template.py
 typst compile template.typ turni.pdf
 
-# Pulizia finale
+# Configurazione iniziale
+rm data.dat turni.run
+mv days.py turns.py ../
+
 cp copy.typ template.typ
 rm copy.typ
+
+# Da aggiungere spostamento nella cartella per invio su telegram
 
 exit
